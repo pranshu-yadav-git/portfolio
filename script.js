2ce56e8600f2e9edf5e5bda54b376c58bf695772
Pranshu Yadav <notforyou.py@gmail.com>
2025-04-14 19:07:39 +0530

    dev site navigation fuctionality

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-// Add animation delay to grid items
 document.addEventListener('DOMContentLoaded', () => {
-    // Animate tech stack items
     const techItems = document.querySelectorAll('.tech-item');
     techItems.forEach((item, index) => {
         item.style.animationDelay = `${0.1 * index}s`;
     });
 
-    // Animate service items
     const serviceItems = document.querySelectorAll('.service-item');
     serviceItems.forEach((item, index) => {
         item.style.animationDelay = `${0.1 * index}s`;
     });
 
-    // Animate contact items
     const contactItems = document.querySelectorAll('.contact-item');
     contactItems.forEach((item, index) => {
         item.style.animationDelay = `${0.1 * index}s`;
     });
+
     const backToTopBtn = document.querySelector(".footer-btn-backToTop");
 
-    //Back to top
     if (backToTopBtn) {
         backToTopBtn.addEventListener("click", function () {
             window.scrollTo({
                 top: 0,
                 behavior: "smooth",
             });
+        });
+    }
+
+    const devSiteBtn = document.getElementById("devSiteBtn");
+    if (devSiteBtn) {
+        devSiteBtn.addEventListener("click", function (e) {
+            e.preventDefault();
+            const password = prompt("Enter the password to access the Dev Site:");
+
+            if (password === "030210") {
+                window.location.href = "index - beta.html";
+            } else {
+                alert("Incorrect password. Access denied.");
+            }
         });
     }
 });
